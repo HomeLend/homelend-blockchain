cbf5e1b18ba4ecf0eb7bad31f7b46d50d40c62dc
Kanat Tulbassiyev <kanat@genesiszero.com>
2018-01-04 19:44:57 +0600

    configuration bugs were fixed.

--- generate.sh
+++ generate.sh
@@ -4,14 +4,14 @@ export CHANNEL_NAME=mainchannel
 
 rm -rf channel-artifacts crypto-config
 mkdir channel-artifacts
-./bin/cryptogen generate --config=./crypto-config.yaml
-./bin/configtxgen -profile MainOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-./bin/configtxgen -profile MainChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCBankMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCBankMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCInsuranceMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCInsuranceMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCAppraiserMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCAppraiserMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCGovernmentMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCGovernmentMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCBuyerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCBuyerMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCSellerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCSellerMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCCreditRatingAgencyMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCCreditRatingAgencyMSP
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCHomelendMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCHomelendMSP
+./new_bin/cryptogen generate --config=./crypto-config.yaml
+./new_bin/configtxgen -profile MainOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+./new_bin/configtxgen -profile MainChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCBankMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCBankMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCInsuranceMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCInsuranceMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCAppraiserMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCAppraiserMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCGovernmentMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCGovernmentMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCBuyerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCBuyerMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCSellerMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCSellerMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCCreditRatingAgencyMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCCreditRatingAgencyMSP
+./new_bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/POCHomelendMSPanchors.tx -channelID $CHANNEL_NAME -asOrg POCHomelendMSP
